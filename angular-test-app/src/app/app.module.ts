ae143614ee0cdc0d0be0d41cdde58e6b439f5d4f
nbsteuv <nbsteuv@gmail.com>
2017-04-11 05:47:29 -0400

    Created warning component with CLI ng generate component command

--- angular-test-app/src/app/app.module.ts
+++ angular-test-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SuccessComponent } from './messages/success/success.component';
+import { WarningComponent } from './messages/warning/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SuccessComponent
+    SuccessComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule,
